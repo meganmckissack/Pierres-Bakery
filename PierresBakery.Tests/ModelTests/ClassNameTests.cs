b2be46b1a61681c07085ef2988d7fbd5d353a85d
Megan McKissack <meganmckissack@gmail.com>
2022-07-01 14:34:36 -0700

    create boilerplate files and structure

--- PierresBakery.Tests/ModelTests/ClassNameTests.cs
+++ PierresBakery.Tests/ModelTests/ClassNameTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProjectName;
 
-namespace ProjectName.Tests
+namespace Bakery.Tests
 {
   [TestClass]
   public class ClassNameTests
--- PierresBakery/Models/ClassName.cs
+++ PierresBakery/Models/ClassName.cs
@@ -1,4 +1,4 @@
-namespace ProjectName
+namespace Bakery
 {
   public class ClassName
   {
